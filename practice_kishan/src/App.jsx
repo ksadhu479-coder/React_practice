793700a9394ce330326a910f6e42196f010cb7cf
KISHAN SADHU <kishansadhusadhu69@gmail.com>
2026-01-02 21:23:39 +0530

    propes_practice

--- practice_kishan/src/App.jsx
+++ practice_kishan/src/App.jsx
@@ -1,10 +1,28 @@
 import Classbase_imlementation from './module/classbase_imlementation';
+
+//for the propes we need to import classname of the file
+import First from './module/props';
+
+
 function App() {
 
 
   return (
     <>
-      <Classbase_imlementation />
+      {/* <Classbase_imlementation /> */}
+
+
+
+
+      {/* here also is default props */}
+      <First />
+
+      
+      <First
+      name = "one"
+      img = "https://tse4.mm.bing.net/th/id/OIP.IDq8cZVcGfgwgkR1Bt0RywHaEo?pid=Api&P=0&h=180"
+      price = "100000"
+      />
     </>
   )
 }
---
+++ practice_kishan/src/module/props.jsx
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+// import PropTypes from 'prop-type';
+
+
+
+class First extends Component{
+    render(){
+        //destructure props
+        const {img,price} = this.props;
+        return(
+            <>
+                 <h1>this is example of the props  <u>{this.props.name}</u> </h1>
+
+                 <h4>Name : {this.props.name}</h4>
+                 <img src={img} alt="" height={100} width={100} />
+                 <h4>Price : {price}</h4>
+            </>
+        );
+    }
+}
+
+
+// default props
+First.defaultProps = {
+    name : "sadhu",
+    img : "https://tse4.mm.bing.net/th/id/OIP.dYEt7HJ1fMzkjAdyU8hLdwHaFj?pid=Api&P=0&h=180",
+    price : "20000"
+}
+
+export default First;
