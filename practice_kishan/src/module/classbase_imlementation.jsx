1971d218d2994c585fb8cbed657c928fdf99d2fc
KISHAN SADHU <kishansadhusadhu69@gmail.com>
2026-01-02 21:10:22 +0530

    classbase implimentation

---
+++ practice_kishan/src/module/classbase_imlementation.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class First extends Component{
+    render(){
+        return(
+            <>
+                 <h1>this is example of the class base concept</h1>
+                 <img src="vita.svg" alt="" height={100} width={100} />
+
+            </>
+        );
+    }
+}
+
+
+export default First;
